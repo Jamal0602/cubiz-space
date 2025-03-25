e5ab2b6cd646e2730580da441df8fcddb76207c2
Jamal Asraf <162672524+Jamal0602@users.noreply.github.com>
2025-03-25 20:50:54 +0530

    Update MainLayout.tsx

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -4,7 +4,6 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import { useLocation } from "react-router-dom";
 import { motion } from "framer-motion";
-import Script from "next/script"
 
 interface MainLayoutProps {
   children: ReactNode;
@@ -16,9 +15,6 @@ const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      
-    <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7483780622360467"
-     crossorigin="anonymous"></Script>
       <motion.main 
         key={location.pathname}
         initial={{ opacity: 0, y: 10 }}
