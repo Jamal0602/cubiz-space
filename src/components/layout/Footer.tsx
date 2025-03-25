a759a12c56a14e96053181d238bdb0547d19f537
Jamal Asraf <162672524+Jamal0602@users.noreply.github.com>
2025-03-25 20:43:51 +0530

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,12 +1,15 @@
 
 import { Link } from "react-router-dom";
 import { Github, Heart, Twitter } from "lucide-react";
+import Script from "next/script"
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   
   return (
     <footer className="bg-white border-t border-gray-100">
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7483780622360467"
+     crossorigin="anonymous"></Script>
       <div className="container mx-auto px-6 py-12">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div className="md:col-span-1">
